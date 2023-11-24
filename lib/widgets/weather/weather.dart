08e73fc14a518bc1323f236f3e9fd431ad37e64f
湯木 悠太 [YUMEMI] / Yuki Yuta <y_yuki@yumemi.co.jp>
2023-11-24 15:08:54 +0900

    yumemi weatherをfetchし、placeholderを描画だしわけするように修正

--- lib/widgets/weather/weather.dart
+++ lib/widgets/weather/weather.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_training/models/weather_condition.dart';
 import 'package:flutter_training/widgets/weather/degrees.dart';
 
 class WeatherWidget extends StatelessWidget {
   const WeatherWidget({
     super.key,
-  });
+    WeatherCondition? weather,
+  }) : _weather = weather;
+
+  final WeatherCondition? _weather;
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
         AspectRatio(
           aspectRatio: 1,
-          child: Placeholder(),
+          child: _weather == null
+              ? const Placeholder()
+              : SvgPicture.asset(_weather.image),
         ),
-        Padding(
+        const Padding(
           padding: EdgeInsets.symmetric(vertical: 16),
           child: DegreesWidget(),
         ),
