38aba7e5f8dba76730ebfc8ab581df2ec11cfe14
湯木 悠太 [YUMEMI] / Yuki Yuta <y_yuki@yumemi.co.jp>
2023-11-27 15:33:38 +0900

    screenを別ファイルに切り出し

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/models/weather_condition.dart';
-import 'package:flutter_training/widgets/weather/weather.dart';
-import 'package:yumemi_weather/yumemi_weather.dart';
+import 'package:flutter_training/weather_screen.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  final _yumemiWeather = YumemiWeather();
-  WeatherCondition? _weather;
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: FractionallySizedBox(
-            widthFactor: 0.5,
-            child: Column(
-              children: [
-                const Spacer(),
-                WeatherWidget(weather: _weather),
-                Flexible(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 80),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: TextButton(
-                            onPressed: () {},
-                            child: const Text('Close'),
-                          ),
-                        ),
-                        Expanded(
-                          child: TextButton(
-                            onPressed: () {
-                              final weather = WeatherCondition.from(
-                                _yumemiWeather.fetchSimpleWeather(),
-                              );
-                              setState(() {
-                                _weather = weather;
-                              });
-                            },
-                            child: const Text('Reload'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+    return const MaterialApp(home: WeatherScreen());
   }
 }
---
+++ lib/weather_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_training/models/weather_condition.dart';
+import 'package:flutter_training/widgets/weather/weather.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
+
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  final _yumemiWeather = YumemiWeather();
+  WeatherCondition? _weather;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          child: Column(
+            children: [
+              const Spacer(),
+              WeatherWidget(weather: _weather),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 80),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text('Close'),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () {
+                            final weather = WeatherCondition.from(
+                              _yumemiWeather.fetchSimpleWeather(),
+                            );
+                            setState(() {
+                              _weather = weather;
+                            });
+                          },
+                          child: const Text('Reload'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
