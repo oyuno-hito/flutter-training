f3d9d6226f6c519feb226ebf598bfa6cdf60171b
湯木 悠太 [YUMEMI] / Yuki Yuta <y_yuki@yumemi.co.jp>
2023-12-08 15:26:43 +0900

    最低気温, 最高気温をfetchした値を返すようにコンポーネントを修正

--- lib/features/weather/components/degrees.dart
+++ lib/features/weather/components/degrees.dart
@@ -3,16 +3,25 @@ import 'package:flutter/material.dart';
 class DegreesWidget extends StatelessWidget {
   const DegreesWidget({
     super.key,
-  });
+    int? maxTemperature,
+    int? minTemperature,
+  })  : _maxTemperature = maxTemperature,
+        _minTemperature = minTemperature;
+
+  final int? _maxTemperature;
+  final int? _minTemperature;
+
+  static const _defalutValue = '**';
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+
     return Row(
       children: [
         Expanded(
           child: Text(
-            '** ℃',
+            '${_minTemperature ?? _defalutValue} ℃',
             textAlign: TextAlign.center,
             style: theme.textTheme.labelLarge?.copyWith(
               color: Colors.blue,
@@ -21,7 +30,7 @@ class DegreesWidget extends StatelessWidget {
         ),
         Expanded(
           child: Text(
-            '** ℃',
+            '${_maxTemperature ?? _defalutValue} ℃',
             textAlign: TextAlign.center,
             style: theme.textTheme.labelLarge?.copyWith(
               color: Colors.red,
