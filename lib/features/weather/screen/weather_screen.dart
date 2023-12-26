6d704944a1283670e444c941772a30bf04152765
湯木 悠太 [YUMEMI] / Yuki Yuta <y_yuki@yumemi.co.jp>
2023-12-26 16:09:39 +0900

    CheckedFromJsonExceptionのimport元を修正

--- lib/features/weather/screen/weather_screen.dart
+++ lib/features/weather/screen/weather_screen.dart
@@ -6,7 +6,7 @@ import 'package:flutter_training/features/weather/components/weather.dart';
 import 'package:flutter_training/features/weather/exceptions/app_exception.dart';
 import 'package:flutter_training/features/weather/model/weather_forecast.dart';
 import 'package:flutter_training/features/weather/model/weather_forecast_request.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 class WeatherScreen extends StatefulWidget {
