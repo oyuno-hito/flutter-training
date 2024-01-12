79d15b6f567bed16c169d884f60bf09a923e8963
湯木 悠太 [YUMEMI] / Yuki Yuta <y_yuki@yumemi.co.jp>
2024-01-12 16:06:20 +0900

    weather_screen_stateの不要なコメントを削除(rebase予定)

--- lib/features/weather/model/weather_screen_state.dart
+++ lib/features/weather/model/weather_screen_state.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_training/features/weather/model/weather_forecast.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-// part 'weather_screen_state.g.dart';
 part 'weather_screen_state.freezed.dart';
 
 @freezed
